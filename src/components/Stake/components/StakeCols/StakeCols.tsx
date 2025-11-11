715da317d2b40bb4f7cd9a846d645d092940bcba
Colombia Staking <128503013+colombiastaking@users.noreply.github.com>
2025-11-11 17:19:38 +0100

    50millons gas withdraw

--- src/components/Stake/components/StakeCols/StakeCols.tsx
+++ src/components/Stake/components/StakeCols/StakeCols.tsx
@@ -20,7 +20,7 @@ const COLS_TOKEN_ID = 'COLS-9d91b7';
 const COLS_TOKEN_ID_HEX = '434f4c532d396439316237';
 const STAKE_CONTRACT = 'erd1qqqqqqqqqqqqqpgqjhn0rrta3hceyguqlmkqgklxc0eh0r5rl3tsv6a9k0';
 const GAS_LIMIT = 15_000_000;
-const WITHDRAW_GAS_LIMIT = 40_000_000;
+const WITHDRAW_GAS_LIMIT = 50_000_000;
 const STAKE_METHOD_HEX = '7374616b65'; // "stake" in hex
 const FIXED_HEX_ADDRESS = '00000000000000000500f5ae3a400dae272bd254689fd5a44f88e3f2949e5787';
 
