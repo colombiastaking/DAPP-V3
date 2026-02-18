ce4dbffb792d653b7fbd42815fd28fde9ad92795
SebastienLombard <sebastien@colombia-staking.com>
2026-02-17 20:31:38 -0500

    Revert: Original unlock page

--- src/pages/Unlock/Unlock.tsx
+++ src/pages/Unlock/Unlock.tsx
@@ -4,6 +4,8 @@ import { UnlockPanelManager } from '@multiversx/sdk-dapp/out/managers/UnlockPane
 import { useGetLoginInfo } from '@multiversx/sdk-dapp/out/react/loginInfo/useGetLoginInfo';
 import { routeNames } from 'routes';
 
+import { MultiversX } from 'assets/MultiversX';
+
 import styles from './styles.module.scss';
 
 export const Unlock = () => {
@@ -32,17 +34,11 @@ export const Unlock = () => {
   };
 
   return (
-    <div className={styles.unlock}>
-      <div className={styles.wrapper}>
-        {/* Custom Colombia Staking Logo */}
-        <div className={styles.logo}>
-          <img 
-            src="/colombia-staking-logo.svg" 
-            alt="Colombia Staking" 
-            style={{ width: 48, height: 48, objectFit: 'contain' }}
-          />
+    <div className={styles.unlock} style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div className={styles.wrapper} style={{ maxWidth: 520, width: '100%', margin: '0 auto', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+        <div className={styles.logo} style={{ marginBottom: 28 }}>
+          <MultiversX />
         </div>
-        
         <div style={{ textAlign: 'center', marginBottom: 28 }}>
           <strong className={styles.heading} style={{ fontSize: 28, color: '#62dbb8', display: 'block', marginBottom: 14, fontFamily: 'Lustria, serif' }}>
             Colombia Staking
